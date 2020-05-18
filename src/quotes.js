c696a43eb18023f6a8d4b2c11bc5f6c4103264e3
TheMadMike <m.gibas2407@gmail.com>
2020-05-18 22:33:50 +0200

    Add a check to ensure that id is a number before sendind response

--- src/quotes.js
+++ src/quotes.js
@@ -17,10 +17,16 @@ router.get('/daily', (request, response) => {
 });
 
 router.get('/:id', (request, response) => {
-  response.json({
-    quote: `quote no. ${request.params.id}`,
-    author: 'Unknown'
-  });
+  // eslint-disable-next-line no-restricted-globals
+  if (isNaN(request.params.id)) {
+    response.status(400);
+    response.send(`Error: ${response.statusCode}`);
+  } else {
+    response.json({
+      quote: `quote no. ${request.params.id}`,
+      author: 'Unknown'
+    });
+  }
 });
 
 module.exports = router;
