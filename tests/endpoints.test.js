a65d4d7afb00965b88a762e0289918a669c9cafe
TheMadMike <m.gibas2407@gmail.com>
2020-05-22 21:36:13 +0200

    Add tests for /daily endpoint

--- tests/endpoints.test.js
+++ tests/endpoints.test.js
@@ -2,6 +2,12 @@ const request = require('supertest');
 const app = require('../src/app');
 const db = require('../src/db.js');
 
+const expectValidQuoteData = (response) => {
+  expect(response.status).toBe(200);
+  expect(response.body.quote).toBeDefined();
+  expect(response.body.author).toBeDefined();
+};
+
 describe('GET /', () => {
   it('responds with json containing info about the project', async (done) => {
     const response = await request(app).get('/');
@@ -35,9 +41,7 @@ describe('GET /:id', () => {
 
   it('responds with proper json data when id is valid and in range', async (done) => {
     const response = await request(app).get('/1').expect('Content-Type', /json/);
-    expect(response.status).toBe(200);
-    expect(response.body.quote).toBeDefined();
-    expect(response.body.author).toBeDefined();
+    expectValidQuoteData(response);
     done();
   });
 });
@@ -45,9 +49,22 @@ describe('GET /:id', () => {
 describe('GET /random', () => {
   it('responds with proper json data', async (done) => {
     const response = await request(app).get('/random').expect('Content-Type', /json/);
-    expect(response.status).toBe(200);
-    expect(response.body.quote).toBeDefined();
-    expect(response.body.author).toBeDefined();
+    expectValidQuoteData(response);
+    done();
+  });
+});
+
+describe('GET /daily', () => {
+  it('responds with proper json data', async (done) => {
+    const response = await request(app).get('/daily').expect('Content-Type', /json/);
+    expectValidQuoteData(response);
+    done();
+  });
+  it('responds with the same data when requested twice over a short time span', async (done) => {
+    let response = await request(app).get('/daily');
+    const firstID = response.body.id;
+    response = await request(app).get('/daily');
+    expect(firstID).toBe(response.body.id);
     done();
   });
 });
